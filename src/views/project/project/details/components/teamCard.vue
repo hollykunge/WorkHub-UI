03ba05470230a4394e6c12beff19f17b8bafe700
jihainan <alwaysbegoing@163.com>
2018-04-16 19:01:07 +0800

    项目团队（1）

---
+++ src/views/project/project/details/components/teamCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-card class="card-box">
+    <div slot="header">
+      <span style="line-height: 36px;">任务一相关团队</span>
+      <el-button style="float: right;" type="primary">编辑</el-button>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'teamCard',
+  props: {
+    teamList: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      test: undefined
+    }
+  }
+
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+.card-box {
+  height: 275px;
+  width: 90%;
+}
+</style>
