0dc9f6b19e559f6a4aa0a3027c316005f9b5f971
jihainan <alwaysbegoing@163.com>
2018-04-23 18:11:35 +0800

    修改sidebar的图标

--- src/views/layout/SidebarItem.vue
+++ src/views/layout/SidebarItem.vue
@@ -10,14 +10,16 @@
 		</router-link> -->
 			<el-submenu :index="item.title" :key="item.title">
 				<template slot="title">
-					<icon-svg v-if='item.icon' :icon-class="item.icon"></icon-svg>
+					<!-- <icon-svg v-if='item.icon' :icon-class="item.icon"></icon-svg> -->
+					<icon v-if='item.icon' :name="item.icon"></icon>
 					<span>{{item.title}}</span>
 				</template>
 				<template v-for="child in item.children">
 					<sidebar-item class='nest-menu' v-if='child.children&&child.children.length>0' :routes='[child]' :key="child.title"> </sidebar-item>
 					<router-link v-else :to="'/'+item.code+'/'+child.code" :key="child.code">
 						<el-menu-item :index="item.code+'/'+child.code">
-							<icon-svg v-if='child.icon' :icon-class="child.icon"></icon-svg>
+							<!-- <icon-svg v-if='child.icon' :icon-class="child.icon"></icon-svg> -->
+							<icon v-if='child.icon' :name="child.icon"></icon>
 							<span>{{child.title}}</span>
 						</el-menu-item>
 					</router-link>
