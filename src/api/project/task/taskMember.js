61b291a45d724101808ec0cc8f7dfca92d2a38cd
jihainan <alwaysbegoing@163.com>
2018-08-13 17:30:07 +0800

    添加任务成员的后台接口

---
+++ src/api/project/task/taskMember.js
@@ -0,0 +1,35 @@
+import fetch from 'utils/fetch'
+
+// 查看当前任务下的任务成员
+export function getTaskMember(query) {
+  return fetch({
+    url: '/api/project/map/page',
+    method: 'get',
+    params: query
+  })
+}
+
+// 关联已选择用户
+export function associateUser(data) {
+  return fetch({
+    url: '/api/project/map',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改成员权限
+export function modifyMemberPermission(userId) {
+  return fetch({
+    url: '/api/project/map/' + userId,
+    method: 'put'
+  })
+}
+
+// 删除成员
+export function deleteMember(userId) {
+  return fetch({
+    url: '/api/project/map/' + userId,
+    method: 'delete'
+  })
+}
