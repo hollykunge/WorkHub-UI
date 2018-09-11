495b1114066c286848798b21e0e5a682370bda32
jihainan <alwaysbegoing@163.com>
2018-09-11 10:43:33 +0800

    修改bug

--- src/views/dashboard/components/activityList.vue
+++ src/views/dashboard/components/activityList.vue
@@ -3,7 +3,7 @@
     <div class="activity-list-container">
       <div class="message-container">
         <!-- 使用统一的数据展示的模板 -->
-        <div class="message-list" v-for="(message, index) in displayedList" :key="index" v-if="!message.isPrivate || message.userId == userId">
+        <div class="message-list" v-for="(message, index) in messageList" :key="index" v-if="!message.isPrivate || message.actUserId == userId">
           <span class="message-list-avatar">
             <Avatar :username="message.actUserName" :size="35" :lighten="200" style=""></Avatar>
           </span>
@@ -52,13 +52,14 @@
             </div>
           </div>
         </div>
-        <div v-if="displayedList.length == 0">
+        <div v-if="total == 0">
           <!-- 暂无动态 -->
           <h4>暂无动态</h4>
         </div>
       </div>
       <div class="load-more">
         <el-button @click="loadMore" v-if="isLoadMore">加载更多</el-button>
+        <el-button @click="loadMore" v-if="!isLoadMore && total != 0">没有更多了</el-button>
       </div>
     </div>
   </div>
@@ -73,60 +74,60 @@ export default {
   data() {
     return {
       messageList: [],
-      displayedList: [],
       isLoadMore: false,
-      loadMoreCounter: 0,
       listQuery: {
         limit: 20,
         page: 1,
         dayBack: 7
-      }
+      },
+      total: 0
     }
   },
   computed: {
     ...mapGetters(['userId'])
   },
   created() {
-    this.listQuery.crtUser = this.userId
+    this.listQuery.actUserId = this.userId
     this.getMessageList()
   },
   mounted() {
   },
   methods: {
     getMessageList() {
+      const currentCounter = this.total
       getActivity(this.listQuery).then(res => {
         console.log(res.data)
-        if (res.data.total === 0) {
-          this.messageList = [
-            // {userId, optType, actUserId, actUserName, repoId, repoUserName, repoName, refName, isPrivate, createdUnix, content}
-            // 1创建任务 2删除任务 3修改任务 4提交数据给任务 5任务人员变更 6数据合并
-            { userId: 1, optType: 1, actUserId: 1, actUserName: '赵向洋', repoId: 1, repoUserName: '赵向洋', repoName: '新型仿真机器人', refName: '数据名称', isPrivate: false, createdUnix: 'none', content: '遥感空间信息可信现场v现场v现在才v秩序只需啊倒萨顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶度理论与关键技术', crtTime: '1535099458000' },
-            { userId: 2, optType: 2, actUserId: 1, actUserName: '孙星星', repoId: 1, repoUserName: '孙星星', repoName: '工程效能', refName: '数据名称', isPrivate: false, createdUnix: 'none', content: '新型仿真机器人', crtTime: '1535099458000' },
-            { userId: 3, optType: 3, actUserId: 1, actUserName: '朱偏右', repoId: 1, repoUserName: '朱偏右', repoName: '卫星通导及系统融合产业化', refName: '数据名称', isPrivate: false, createdUnix: 'none', content: '卫星通导及系统融合产业化', crtTime: '1535099458000' },
-            { userId: 4, optType: 4, actUserId: 1, actUserName: '王梦源', repoId: 1, repoUserName: '王梦源', repoName: '智能生产', refName: '数据名称', isPrivate: false, createdUnix: 'none', content: '智能生产', crtTime: '1535099458000' },
-            { userId: 5, optType: 5, actUserId: 1, actUserName: '胡  超', repoId: 1, repoUserName: '胡  超', repoName: '新型仿真机器人', refName: '数据名称', isPrivate: false, createdUnix: 'none', content: '工业机器人', crtTime: '1535099458000' },
-            { userId: 6, optType: 6, actUserId: 1, actUserName: '钱海波', repoId: 1, repoUserName: '钱海波', repoName: '工业机器人', refName: '数据名称', isPrivate: false, createdUnix: 'none', content: '安全防御关键技术', crtTime: '1535099458000' },
-            { userId: 3, optType: 4, actUserId: 1, actUserName: '张童飞', repoId: 1, repoUserName: '张童飞', repoName: '八月迭代', refName: '数据名称', isPrivate: true, createdUnix: 'none', content: '八月迭代', crtTime: '1535099458000' },
-            { userId: 1, optType: 5, actUserId: 1, actUserName: '米思伟', repoId: 1, repoUserName: '米思坤', repoName: '安全防御关键技术', refName: '数据名称', isPrivate: true, createdUnix: 'none', content: '工程信息化', crtTime: '1535099458000' }]
-            this.loadMore()
-        } else {
-          this.messageList = res.data.rows
-          this.loadMore()
+        // {userId, optType, actUserId, actUserName, repoId, repoUserName, repoName, refName, isPrivate, createdUnix, content}
+        // 1创建任务 2删除任务 3修改任务 4提交数据给任务 5任务人员变更 6数据合并
+        // { userId: 1, optType: 1, actUserId: 1, actUserName: '赵向洋', repoId: 1, repoUserName: '赵向洋', repoName: '新型仿真机器人', refName: '数据名称', isPrivate: false, createdUnix: 'none', content: '遥感空间信息可信现场v现场v现在才v秩序只需啊倒萨顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶度理论与关键技术', crtTime: '1535099458000' },
+        // { userId: 2, optType: 2, actUserId: 1, actUserName: '孙星星', repoId: 1, repoUserName: '孙星星', repoName: '工程效能', refName: '数据名称', isPrivate: false, createdUnix: 'none', content: '新型仿真机器人', crtTime: '1535099458000' },
+        // { userId: 3, optType: 3, actUserId: 1, actUserName: '朱偏右', repoId: 1, repoUserName: '朱偏右', repoName: '卫星通导及系统融合产业化', refName: '数据名称', isPrivate: false, createdUnix: 'none', content: '卫星通导及系统融合产业化', crtTime: '1535099458000' },
+        // { userId: 4, optType: 4, actUserId: 1, actUserName: '王梦源', repoId: 1, repoUserName: '王梦源', repoName: '智能生产', refName: '数据名称', isPrivate: false, createdUnix: 'none', content: '智能生产', crtTime: '1535099458000' },
+        // { userId: 5, optType: 5, actUserId: 1, actUserName: '胡  超', repoId: 1, repoUserName: '胡  超', repoName: '新型仿真机器人', refName: '数据名称', isPrivate: false, createdUnix: 'none', content: '工业机器人', crtTime: '1535099458000' },
+        // { userId: 6, optType: 6, actUserId: 1, actUserName: '钱海波', repoId: 1, repoUserName: '钱海波', repoName: '工业机器人', refName: '数据名称', isPrivate: false, createdUnix: 'none', content: '安全防御关键技术', crtTime: '1535099458000' },
+        // { userId: 3, optType: 4, actUserId: 1, actUserName: '张童飞', repoId: 1, repoUserName: '张童飞', repoName: '八月迭代', refName: '数据名称', isPrivate: true, createdUnix: 'none', content: '八月迭代', crtTime: '1535099458000' },
+        // { userId: 1, optType: 5, actUserId: 1, actUserName: '米思伟', repoId: 1, repoUserName: '米思坤', repoName: '安全防御关键技术', refName: '数据名称', isPrivate: true, createdUnix: 'none', content: '工程信息化', crtTime: '1535099458000' }]
+        if (res.data.total === currentCounter) {
+          this.loadMore = false
+          return
         }
-      }, rej => {
-        console.log('获取最新动态列表失败')
+        this.messageList = res.data.rows
+        this.total = res.data.total
+        this.loadMore = true
       })
     },
     loadMore() {
-      const Counter = 3 + 3 * this.loadMoreCounter
-      if (this.messageList.length <= Counter) {
-        this.displayedList = this.messageList
-        this.isLoadMore = false
-      } else {
-        this.displayedList = this.messageList.slice(0, Counter)
-        this.isLoadMore = true
-      }
-      this.loadMoreCounter++
+      // const Counter = 3 + 3 * this.loadMoreCounter
+      // if (this.messageList.length <= Counter) {
+      //   this.displayedList = this.messageList
+      //   this.isLoadMore = false
+      // } else {
+      //   this.displayedList = this.messageList.slice(0, Counter)
+      //   this.isLoadMore = true
+      // }
+      // this.loadMoreCounter++
+      this.listQuery.limit += 5
+      this.getMessageList()
     },
     getSubstring(originalString) {
       const stringLength = originalString.length
